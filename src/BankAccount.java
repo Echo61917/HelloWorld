d3e1085aa1ca52bc0a199cc9381bec1ed7be1669
cramirez17 <coldflames6927@gmail.com>
2022-02-15 12:40:51 -0800

    WIP classes and homework

---
+++ src/BankAccount.java
@@ -0,0 +1,12 @@
+import java.util.GregorianCalendar;
+
+public class BankAccount {
+        private int accountNumber,balance;
+        private double annualInterestRate;
+        private GregorianCalendar dateCreated;
+
+        public BankAccount()
+        {
+
+        }
+}
---
+++ src/Date.java
@@ -0,0 +1,17 @@
+import java.util.GregorianCalendar;
+
+public class Date {
+    public static void main(String[] args)
+    {
+        GregorianCalendar aYearAgo = new GregorianCalendar(2021, 2, 15);
+        GregorianCalendar today = new GregorianCalendar();
+
+        float balance = 2000.00f;
+        float rate = 0.05f;
+        int yearSpanned = today.get(GregorianCalendar.YEAR) - aYearAgo.get(GregorianCalendar.YEAR);
+
+        balance = balance + balance*rate*yearSpanned;
+        System.out.println("New Balance: " + balance);
+        System.out.println(yearSpanned);
+    }
+}
