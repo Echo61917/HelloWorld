d65b1743b19ca307c3c01d9eeef56f56808d3803
Echo61917 <98851748+Echo61917@users.noreply.github.com>
2022-04-07 22:06:32 -0700

    Homework

--- src/Week6_DataStructures/ParseJavaFile.java
+++ src/Week6_DataStructures/ParseJavaFile.java
@@ -8,7 +8,7 @@ public class ParseJavaFile {
     public static void main(String[] args)
     {
         Stack<String> balance = new Stack<>();
-        File f = new File("Car.java");
+        File f = new File("C://Users//Carlos//IdeaProjects//GitHub//src//Week5_Generics//Car.java");
         try
         {
             Scanner scan = new Scanner(f);
@@ -21,14 +21,22 @@ public class ParseJavaFile {
             Scanner scan2 = new Scanner(f);
             while(scan2.hasNext())
             {
-                String nextToken = scan.next();
+                String nextToken = scan2.next();
                 if(nextToken.contains("}"))
                     balance.pop();
+            }
+            if(balance.isEmpty())
+            {
+                System.out.println("File braces are balanced.");
+            }
+            else
+            {
+                System.out.println("File braces are not balanced, there is an extra brace.");
             }
         }
         catch(FileNotFoundException e)
         {
-
+            System.out.println("File not found.");
         }
     }
 }
