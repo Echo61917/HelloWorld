b1683b12f216e6ab9741e79f8b2d29bcf9a5fedc
Echo61917 <98851748+Echo61917@users.noreply.github.com>
2022-04-26 11:56:06 -0700

    update on homework

---
+++ src/Week6_DataStructures/PriorityQueueExercise2.java
@@ -0,0 +1,30 @@
+package Week6_DataStructures;
+
+import Week5_Generics.Car;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class PriorityQueueExercise2 {
+    public static void main(String[] args)
+    {
+        PriorityQueue<Car> carList = new PriorityQueue<>(Collections.reverseOrder());
+
+        Car car1 = new Car("Honda", "Civic", 2004);
+        Car car2 = new Car("Ford", "Ranger", 1997);
+        Car car3 = new Car("Kia", "Rio", 2013);
+        Car car4 = new Car("Rolls Royce", "Wraith", 2022);
+        Car car5 = new Car("Tesla", "Y", 2022);
+
+        carList.offer(car1);
+        carList.offer(car2);
+        carList.offer(car3);
+        carList.offer(car4);
+        carList.offer(car5);
+
+        while(carList.size() > 0)
+        {
+            System.out.println(carList.remove() + " ");
+        }
+    }
+}
