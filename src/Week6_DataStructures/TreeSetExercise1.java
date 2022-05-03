c7e7b531740a2552c915ec22bf6e768f356507a1
Echo61917 <98851748+Echo61917@users.noreply.github.com>
2022-05-03 10:16:08 -0700

    Homework

--- src/Week6_DataStructures/TreeSetExercise1.java
+++ src/Week6_DataStructures/TreeSetExercise1.java
@@ -2,10 +2,10 @@ package Week6_DataStructures;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeSet;
 
-public class SetsNMaps_CollabNotes {
+public class TreeSetExercise1 {
     public static void main(String[] args)
     {
         String [] source = readFileIntoArray("C://Users//Carlos//IdeaProjects//GitHub//src//Week6_DataStructures//ReadingandAnalyticalSkill.txt");
@@ -19,7 +19,7 @@ public class SetsNMaps_CollabNotes {
             File f = new File(filename);
             Scanner scan = new Scanner(f);
 
-            ArrayList<String> text = new ArrayList<>();
+            TreeSet<String> text = new TreeSet<>();
             while (scan.hasNext()) {
                 text.add(scan.next());
             }
