322a5273515e65f5b0d8ecc1a323dcbbef8f1665
Echo61917 <98851748+Echo61917@users.noreply.github.com>
2022-06-08 18:25:58 -0700

    Homework

--- src/HangmanCode/WordBank.java
+++ src/HangmanCode/WordBank.java
@@ -3,6 +3,7 @@ package HangmanCode;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -12,21 +13,34 @@ public class WordBank {
     {
         HashMap<String, String> wordBank = new HashMap<>();
 
-        String[] words = readFileIntoArray("C://Users//cramirez17//Documents//GitHub//HelloWorld//src//HangmanCode//mammals.csv");
+        String[] words = readFileIntoArray("C://Users//Carlos//IdeaProjects//GitHub//src//HangmanCode//mammals.csv");
+        System.out.println(Arrays.toString(words));
         for(int i = 0; i < words.length; i++)
         {
-            String key = words[i].toLowerCase();
+//            String[] split = words[i].toLowerCase().split(",");
+//            String key1 = split[0];
+//            String value1 = split[1];
+//            wordBank.put(key1, value1);
 
-            if(key.length() > 0)
-                if(!wordBank.containsKey(key)){
-                    wordBank.put(key, "");
-                }
-                else{
-                    String hint = wordBank.get(key);
-                    wordBank.put(key, hint);
-                }
+            String key = words[i].toLowerCase();
+            String[] split = key.split(",");
+            String key1 = split[0];
+            String value = split[1];
+            wordBank.put(key1, value);
+//            if(key.length() > 0)
+//                if(!wordBank.containsKey(key)){
+//                    wordBank.put(key, "");
+//                }
+//                else{
+//                    String hint = wordBank.get(key);
+//                    wordBank.put(key, hint);
+//                }
         }
         wordBank.forEach((k, v) -> System.out.println(k + "\t" + v));
+
+       // String animal = wordBank.put("goat","mammal");
+
+        //System.out.println(wordBank);
 
     }
 
